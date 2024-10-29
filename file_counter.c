ea06c4a5827682ad201c5e1d7c183c9fedecd802
Aleksandr Pavozkov <99251771+apavozkov@users.noreply.github.com>
2024-10-30 01:53:46 +0300

    Update file_counter.c

--- file_counter.c
+++ file_counter.c
@@ -8,7 +8,7 @@
 int main() {
   DIR *dir; // Определяем указатели на структуры для сбора инфы
   struct dirent *entry;
-  struct statfileStat;
+  struct stat fileStat;
 
   dir = opendir("."); // Открываем текущую директорию
   if (dir == NULL) {
@@ -17,10 +17,15 @@ int main() {
   }
 
   // Делаем счётчики
-  int file_count = 0;
+  int regular_count = 0;
   int dir_count = 0;
-  int other_count = 0;
-
+  int chardevice_count = 0;
+  int blockdevice_count = 0;
+  int namedpipe_count = 0;
+  int socket_count = 0;
+  int symlink_count = 0;
+  int unknown_count = 0;
+  
   while ((entry = readdir(dir)) != NULL) { // Перебираем все записи в директории, пока они есть
     if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) { // Игнорируем текущую и родительскую директории
       continue;
@@ -32,21 +37,36 @@ int main() {
     }
 
     // Через макросы sys/stat классифицируем содержимое
-    if (S_ISREG(fileStat.st_mode)) { // Проверяем, является ли файлом
-      file_count++;
-    } else if (S_ISDER(fileStat.st_mode)) { // Проверяем, является ли директорией
+    if (S_ISREG(fileStat.st_mode)) { // Проверяем, является ли обычным файлом
+      regular_count++;
+    } else if (S_ISDIR(fileStat.st_mode)) { // Проверяем, является ли директорией
       dir_count++;
-    } else { // Записываем, если не является обычным файлом или директорией
-      other_count++;
+    } else if (S_ISCHR(fileStat.st_mode)) { // Проверяем, является ли символьным файлом устройства
+      chardevice_count++;
+    } else if (S_ISBLK(fileStat.st_mode)) { // Проверяем, является ли блочным файлом устройства
+      blockdevice_count++;
+    } else if (S_ISFIFO(fileStat.st_mode)) { // Проверяем, является ли именованным каналом
+      namedpipe_count++;
+    } else if (S_ISSOCK(fileStat.st_mode)) { // Проверяем, является ли сокетом
+      socket_count++;
+    } else if (S_ISLNK(fileStat.st_mode)) { // Проверяем, является ли символической ссылкой
+      symlink_count++;
+    } else { // Записываем, если не удалось определить тип
+      unknown_count++;
     }
   }
 
   closedir(dir); 
 
   // Выводим результат
-  printf("Обычных файлов: %d\n", file_count);
-  printf("Директорий: %d\n", dir_count);
-  printf("Других типов файлов: %d/n", other_count);
+  printf("Обычныe файлы: %d\n", regular_count);
+  printf("Директории: %d\n", dir_count);
+  printf("Символьные файлы устройств: %d/n", chardevice_count);
+  printf("Блочные файлы устройств: %d/n", blockdevice_count);
+  printf("Именованные каналы: %d/n", namedpipe_count);
+  printf("Сокеты: %d/n", socket_count);
+  printf("Символические ссылки: %d/n", symlink_count);
+  printf("Неопознанные: %d/n", unknown_count);
 
   return 0;
 }
